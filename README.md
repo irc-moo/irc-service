6bbce95c1abe5f725498e34bfef2195ea193388c
Olivier Palta <oliviersinbox@gmail.com>
2015-11-15 19:02:02 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# irc-moo irc service
+
+![flowchart](http://puu.sh/ln12Y/b4f44f2fa7.png)
---
+++ index.js
@@ -0,0 +1,38 @@
+var net = require('net');
+var http = require('http');
+var fs = require('fs');
+var MuxDemux = require('mux-demux');
+var websocket = require('websocket-stream');
+
+var httpServer = http.createServer();
+httpServer.listen(5000, function() {
+  console.log('http server is live');
+});
+
+var websocketClients = {};
+
+var wss = websocket.createServer({server: httpServer}, handleWsClient);
+
+function handleWsClient(clientStream) {
+  console.log('websocket client connected');
+
+  var meta = 'main';
+  var a = MuxDemux();
+  a.createWriteStream(meta);
+  a.createStream(meta);
+
+  clientStream.pipe(a);
+  b.pipe();
+  // var ircStream = net.connect({
+  //   port: 6667,
+  //   host: 'irc.freenode.org'
+  // });
+  // ircStream.pipe(clientStream).pipe(ircStream);
+  var needle = 'foobar';
+
+  var demuxStream = clientStream
+    .pipe(demuxClientStream);
+
+  demuxClientStream.createWriteStream
+  //.pipe(StreamSearch(new StreamSearch(needle)));
+}
